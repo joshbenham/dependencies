5244568aa0a29ea1e345105e3766764885902d88
Josh Benham <joshbenham@gmail.com>
2018-02-16 23:24:25 +1100

    Created a post-hook system

--- install.sh
+++ install.sh
@@ -33,6 +33,13 @@ for package in packages/*; do
 done
 
 
+# Post Hooks to run after Packages
+for hook in post-hooks/*; do
+    _msg "Running $hook post hooks"
+    source "$hook"
+done
+
+
 _msg
 _msg "And done!"
 _msg
