86ffe88fa4b674d0641017009c2339c9959d35dc
Josh Benham <joshbenham@gmail.com>
2018-02-16 23:14:20 +1100

    Added a way of installing Packages and added i3-gaps and vim

--- install.sh
+++ install.sh
@@ -23,11 +23,17 @@ fi
 if [[ ! -d $DEPENDENCIES ]]; then
     _msg "Installing dependencies repository"
     git clone http://github.com/joshbenham/dependencies $DEPENDENCIES
+    cd $DEPENDENCIES
 else 
     _msg "Upgrading dependencies repository"
     cd $DEPENDENCIES
     git pull origin master
 fi
+
+for f in packages/*; do
+    _msg "Installing $f packages"
+    source "$f"
+done
 
 _msg
 _msg "And done!"
---
+++ packages/system.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+packages=(
+i3-gaps
+vim
+)
+
+sudo pacman -Sy ${packages[@]}
