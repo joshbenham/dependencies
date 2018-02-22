d09981efee287ee10a097512c658cf129e331ed9
Josh Benham <joshbenham@gmail.com>
2018-02-22 15:39:38 +1100

    Testing a way of setting up fisher

--- post-hooks/02-display.sh
+++ post-hooks/02-display.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 
 FILENAME="/etc/systemd/system/display-manager.service"
-if [[ ! -f "$FILENAME" ]]; then
+if [[ ! -e "$FILENAME" ]]; then
     _msg "Setting up SDDM"
     sudo ln -sf /usr/lib/systemd/system/sddm.service "$FILENAME"
 fi
 
+
 _msg "Changing default shell to FISH"
 chsh -s $(which fish)
+
+
+FILENAME="~/.config/fish/functions/fisher.fish"
+if [[ ! -e "$FILENAME" ]]; then
+    _msg "Installing Fisher"
+    curl -Lo "$FILENAME" --create-dirs https://git.io/fisher
+    source "$FILENAME"
+fi
+
+
+_msg "Installing Fisher Packages"
+fisher install fzf
+fisher install edc/bass
+fisher install hauleth/agnoster
+fisher up
